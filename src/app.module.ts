c69762d5f4a494be58fded9c6ecf615b4d8499bf
ksatendar <ksatendar@gmail.com>
2024-07-17 20:45:48 +0530

    backend updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,1 +1,12 @@
-    
+import { Module } from "@nestjs/common";
+import { UsersModule } from './users/users.module';
+import { DatabaseProviders } from 'shared-orm-library/src/database.providers';
+
+@Module({
+    imports: [
+        DatabaseProviders,
+        UsersModule
+    ]
+})
+
+export class AppModule {}
---
+++ src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/Typeorm";
+import { User } from "shared-orm-library/src/entities/user.entity";
+import { UsersService } from "./users.service";
+import { UsersController } from "./users.controller";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([User])],
+    providers: [UsersService],
+    controllers: [UsersController]
+})
+
+export class UsersModule {}
